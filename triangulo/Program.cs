bba90a829d903c93a6e7706ff9e40fb85c39987b
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-23 21:45:42 -0400

    criacao da classe triangulo

--- triangulo/Program.cs
+++ triangulo/Program.cs
@@ -3,31 +3,13 @@
 namespace triangulo
 {
     class Program
-            {
+    {
         protected static void Main(string[] args)
         {
-            int ladoA;
-            int ladoB;
-            int ladoC;
-
-            string texto;
-
-            Console.WriteLine("Digite um valor para o lado A: ");
-            ladoA = Int32.Parse(Console.ReadLine());
-            texto = "LadoA: " + ladoA;
-            //Console.WriteLine(texto);
-            
-            Console.WriteLine("Digite um valor para o lado B: ");
-            ladoB = Int32.Parse(Console.ReadLine());
-            texto += "\nLadoB: " + ladoB;
-            //Console.WriteLine(texto);
-
-            Console.WriteLine("Digite um valor para o lado C: ");
-            ladoC = Int32.Parse(Console.ReadLine());
-            texto += "\nLadoC: " + ladoC;
-            Console.WriteLine(texto);
-
-
+            Triangulo t = new Triangulo();
+            t.ladoA = 12;
+            t.ladoB = 13;
+            t.ladoC = 14;
         }
     }
 }
