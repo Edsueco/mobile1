c8f74de7705999d7860a20f823aaa8c3a21ddaea
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-23 20:39:31 -0400

    triangulo commit

---
+++ triangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+            {
+        protected static void Main(string[] args)
+        {
+            int ladoA;
+            int ladoB;
+            int ladoC;
+
+            string texto;
+
+            Console.WriteLine("Digite um valor para o lado A: ");
+            ladoA = Int32.Parse(Console.ReadLine());
+            texto = "LadoA: " + ladoA;
+            //Console.WriteLine(texto);
+            
+            Console.WriteLine("Digite um valor para o lado B: ");
+            ladoB = Int32.Parse(Console.ReadLine());
+            texto += "\nLadoB: " + ladoB;
+            //Console.WriteLine(texto);
+
+            Console.WriteLine("Digite um valor para o lado C: ");
+            ladoC = Int32.Parse(Console.ReadLine());
+            texto += "\nLadoC: " + ladoC;
+            Console.WriteLine(texto);
+
+
+        }
+    }
+}
