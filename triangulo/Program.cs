88ee7b59b2df43bd405b9e237365a20d90878400
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-25 20:24:18 -0400

    adicionada a classe quadrado

--- triangulo/Program.cs
+++ triangulo/Program.cs
@@ -7,7 +7,9 @@ namespace triangulo
         protected static void Main(string[] args)
         {
             Triangulo t = new Triangulo();
+            Quadrado q = new Quadrado();
 
+            /*
             Console.WriteLine("Informe o lado A: ");
             t.setladoA(Int32.Parse(Console.ReadLine()));
 
@@ -21,6 +23,26 @@ namespace triangulo
             Console.WriteLine("Lado A: " + t.getladoA());
             Console.WriteLine("Lado B: " + t.getladoB());
             Console.WriteLine("Lado C: " + t.getladoC());
+            */
+
+            Console.WriteLine("Informe os lados do Quadrado: ");
+            Console.WriteLine("lado A: ");
+            q.setladoA(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("lado B: ");
+            q.setladoB(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("lado C: ");
+            q.setladoC(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("lado D: ");
+            q.setladoD(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("\nValores informado pelo usuario:");
+            Console.WriteLine("Lado A: " + q.getladoA());
+            Console.WriteLine("Lado B: " + q.getladoB());
+            Console.WriteLine("Lado C: " + q.getladoC());
+            Console.WriteLine("Lado D: " + q.getladoD());
         }
     }
 }
--- triangulo/Quadrado.cs
+++ triangulo/Quadrado.cs
@@ -3,6 +3,51 @@ namespace triangulo
 {
     public class Quadrado
     {
+        private int ladoA;
+        private int ladoB;
+        private int ladoC;
+        private int ladoD;
+
+        public void setladoA(int LadoA)
+        {
+            ladoA = LadoA;
+        }
+
+        public void setladoB(int LadoB)
+        {
+            ladoB = LadoB;
+        }
+
+        public void setladoC(int LadoC)
+        {
+            ladoC = LadoC;
+        }
+
+        public void setladoD(int LadoD)
+        {
+            ladoD = LadoD;
+        }
+
+        public string getladoA()
+        {
+            return ladoA.ToString();
+        }
+
+        public string getladoB()
+        {
+            return ladoB.ToString();
+        }
+
+        public string getladoC()
+        {
+            return ladoC.ToString();
+        }
+
+        public string getladoD()
+        {
+            return ladoD.ToString();
+        }
+
         public Quadrado()
         {
         }
